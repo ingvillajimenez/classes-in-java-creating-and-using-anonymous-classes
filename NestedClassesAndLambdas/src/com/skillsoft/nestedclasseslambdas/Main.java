ce5684a248597c7a43ca449b05bd9897945710c8
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-28 17:48:05 -0600

    Exploring Nuances of Anonymous Classes in Java

--- NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Main.java
+++ NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Main.java
@@ -4,186 +4,158 @@ import java.util.Objects;
 
 public class Main {
 
-    private static void displayCar(Car car) {
-        System.out.println("*** Displaying car");
-        System.out.println(car);
-    }
-
-    private static void displayHome(Home home) {
-        System.out.println("*** Displaying home");
-        System.out.println(home);
-    }
+//    private static void displayCar(Car car) {
+//        System.out.println("*** Displaying car");
+//        System.out.println(car);
+//    }
+//
+//    private static void displayHome(Home home) {
+//        System.out.println("*** Displaying home");
+//        System.out.println(home);
+//    }
 
     public static void main(String[] args) {
 
-        Car lamborghini = new Car() {
-
+        Condo condo = new Condo("Boston", "Victorian", 678000) {
+        // Anonymous class derives from the concrete Condo class
             @Override
-            public String getMake() {
-                return "Lamborghini";
-            }
-
-            @Override
-            public String getModel() {
-                return "Aventador";
-            }
-
-            @Override
-            public int getPrice() {
-                return 200000;
-            }
-
-            @Override
-            public int getMileage() {
-                return 14;
-            }
-
-            @Override
-            public int hashCode() {
-                return Objects.hash(getMake(), getModel(), getPrice(), getMileage());
-            }
-
-            @Override
-            public boolean equals(Object other) {
-                if (other == null) {
-                    return false;
-                }
-
-                if (!(other instanceof Car)) {
-                    return false;
-                }
-
-                Car otherCar = (Car) other;
-
-                if (otherCar.getMake().equals(getMake()) &&
-                        otherCar.getModel().equals(getModel()) &&
-                        otherCar.getPrice() == getPrice() &&
-                        otherCar.getMileage() == getMileage()) {
-                    return true;
-                }
-
-                return false;
-            }
-
-            @Override
-            public String toString() {
-                return String.format("Make: %s, model: %s, price: %d, mileage: %d",
-                        getMake(), getModel(), getPrice(), getMileage());
+            public String getArchitecturalStyle() {
+                return "Gothic";
             }
         };
 
-        Car camry = new Car() {
-
-            @Override
-            public String getMake() {
-                return "Toyota";
-            }
-
-            @Override
-            public String getModel() {
-                return "Camry";
-            }
-
+        Honda honda = new Honda("Acura", 34000, 24) {
+            // Anonymous class derives from the concrete Honda class
             @Override
             public int getPrice() {
-                return 28000;
-            }
-
-            @Override
-            public int getMileage() {
-                return 24;
-            }
-
-            @Override
-            public int hashCode() {
-                return Objects.hash(getMake(), getModel(), getPrice(), getMileage());
-            }
-
-            @Override
-            public boolean equals(Object other) {
-                if (other == null) {
-                    return false;
-                }
-
-                if (!(other instanceof Car)) {
-                    return false;
-                }
-
-                Car otherCar = (Car) other;
-
-                if (otherCar.getMake().equals(getMake()) &&
-                        otherCar.getModel().equals(getModel()) &&
-                        otherCar.getPrice() == getPrice() &&
-                        otherCar.getMileage() == getMileage()) {
-                    return true;
-                }
-
-                return false;
-            }
-
-            @Override
-            public String toString() {
-                return String.format("Make: %s, model: %s, price: %d, mileage: %d",
-                        getMake(), getModel(), getPrice(), getMileage());
+                return 30000;
             }
         };
 
-        displayCar(lamborghini);
-        displayCar(camry);
+        System.out.println(condo);
+        System.out.println(honda);
 
-        System.out.println("lamborghini.equals(camry): " + lamborghini.equals(camry));
+//        final String make = "Lamborghini"; // explicitly final
+//        final String model = "Aventador"; // explicitly final
+//        final int price = 200000; // explicitly final
+//        final int mileage = 14; // explicitly final
 
-//        Home mansion = new Home() {
+//        String make = "Lamborghini"; // effectively final
+//        String model = "Aventador"; // effectively final
+//        int price = 200000; // effectively final
+//        int mileage = 14; // effectively final
+//
+//        Car lamborghini = new Car() {
 //
 //            @Override
-//            public String getCity() {
-//                return "Palo Alto";
+//            public String getMake() {
+//                return make; // Variable 'make' is accessed from within inner class, needs to be final or effectively final
 //            }
 //
 //            @Override
-//            public int getNumberOfFloors() {
-//                return 2;
-//            }
-//
-//            @Override
-//            public String getArchitecturalStyle() {
-//                return "Victorian";
+//            public String getModel() {
+//                return model; // Variable 'model' is accessed from within inner class, needs to be final or effectively final
 //            }
 //
 //            @Override
 //            public int getPrice() {
-//                return 5755000;
+//                return price; // Variable 'price' is accessed from within inner class, needs to be final or effectively final
 //            }
 //
 //            @Override
-//            public int hashCode() {
-//                return Objects.hash(ge);
+//            public int getMileage() {
+//                return mileage; // Variable 'mileage' is accessed from within inner class, needs to be final or effectively final
 //            }
 //
 //            @Override
 //            public String toString() {
-//                return String.format("City: %s, Number of floors: %s, style: %s, price: %d",
-//                        getCity(), getNumberOfFloors(), getArchitecturalStyle(), getPrice());
+//                return String.format("Make: %s, model: %s, price: %d, mileage: %d",
+//                        getMake(), getModel(), getPrice(), getMileage());
 //            }
 //        };
 //
+//        make = "Lamborghini"; // it is no longer final
+//        model = "Huracan"; // it is no longer final
+//        price = 210000; // it is no longer final
+//        mileage = 16; // it is no longer final
+//
 //        displayCar(lamborghini);
-//        displayHome(mansion);
 
-//        System.out.println(lamborghini.getClass());
-//        System.out.println(mansion.getClass());
+//        Car lamborghini = new Car() {
 //
-//        System.out.println();
-//        System.out.println("Is lamborghini anonymous: " + lamborghini.getClass().isAnonymousClass());
-//        System.out.println("Is mansion anonymous: " + mansion.getClass().isAnonymousClass());
+//            @Override
+//            public String getMake() {
+//                return "Lamborghini";
+//            }
 //
-//        System.out.println();
-//        System.out.println("Is lamborghini an interface: " + lamborghini.getClass().isInterface());
-//        System.out.println("Is mansion an interface: " + mansion.getClass().isInterface());
+//            @Override
+//            public String getModel() {
+//                return "Aventador";
+//            }
 //
-//        System.out.println();
-//        System.out.println("Is lamborghini an object: " + (lamborghini instanceof Object));
-//        System.out.println("Is mansion an object: " + (mansion instanceof Object));
+//            @Override
+//            public int getPrice() {
+//                return 200000;
+//            }
+//
+//            @Override
+//            public int getMileage() {
+//                return 14;
+//            }
+//
+//            public void printDetails() {
+//                System.out.println(String.format("Make: %s, model: %s, price: %d, mileage: %d\n",
+//                        getMake(), getModel(), getPrice(), getMileage());;
+//            }
+//        };
+//
+//        lamborghini.printDetails(); // Cannot resolve method 'printDetails' in 'Car'
+
+//      Car lamborghini = new Car() {
+//
+//          public String make = "Lamborghini";
+//          public String model = "Aventador";
+//          public int price = 200000;
+//          public int mileage = 14;
+//
+////          String make = "Lamborghini";
+////          String model = "Aventador";
+////          int price = 200000;
+////          int mileage = 14;
+//
+//          @Override
+//          public String getMake() {
+//              return make;
+//          }
+//
+//          @Override
+//          public String getModel() {
+//              return model;
+//          }
+//
+//          @Override
+//          public int getPrice() {
+//              return price;
+//          }
+//
+//          @Override
+//          public int getMileage() {
+//              return mileage;
+//          }
+//
+//          @Override
+//          public String toString() {
+//              return String.format("Make: %s, model: %s, price: %d, mileage: %d",
+//                      this.make, this.model, this.price, this.mileage);
+//          }
+//      };
+//
+//      displayCar(lamborghini);
+//
+//      System.out.println("Make: " + lamborghini.make); // Cannot resolve symbol 'make'
+//      System.out.println("Model: " + lamborghini.model); // Cannot resolve symbol 'model'
+//      System.out.println("Price: " + lamborghini.price); // Cannot resolve symbol 'price'
+//      System.out.println("Mileage: " + lamborghini.mileage); // Cannot resolve symbol 'mileage'
     }
 }
 
